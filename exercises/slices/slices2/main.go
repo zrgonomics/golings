cc3307e36277c7fe5a715242c0f114e80246c9eb
zrgonomics <193090173+zrgonomics@users.noreply.github.com>
2025-12-13 23:37:08 -0500

    Solve slices

--- exercises/slices/slices2/main.go
+++ exercises/slices/slices2/main.go
@@ -1,13 +1,14 @@
 // slices2
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
 	names := [4]string{"John", "Maria", "Carl", "Peter"}
-	lastTwoNames := names[] // after figuring out the answer, try with other low/high bounds
+	lastTwoNames := names[2:] // after figuring out the answer, try with other low/high bounds
+	firstTwoNames := names[:2]
 	fmt.Println(lastTwoNames)
+	fmt.Println(firstTwoNames)
 }
